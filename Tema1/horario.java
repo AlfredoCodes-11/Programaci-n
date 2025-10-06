db4a587ce636e2119eeb207c3ea14c5187f4abea
AlfredoCodes-11 <acodsan462@g.educaand.es>
2025-10-06 11:15:53 +0200

    sfs

---
+++ Tema1/horario.java
@@ -0,0 +1,14 @@
+package Tema1;
+
+public class horario {
+    public static void main(String[] args) {
+        System.out.println("Lunes\t\tMartes\t\tMiércoles\tJueves\t\tViernes");
+        System.out.println("PROGR\t\tSIINF\t\tBADAT\t\tSIINF\t\tPROGR");
+        System.out.println("PROGR\t\tSIINF\t\tBADAT\t\tBADAT\t\tPROGR");
+        System.out.println("PROGR\t\tENDES\t\tIPE I\t\tBADAT\t\tBADAT");
+        System.out.println("SIINF\t\tENDES\t\tIPE I\t\tPROGR\t\tBADAT");
+        System.out.println("SIINF\t\tENDES\t\tIPE I\t\tPROGR\t\tLMSGI");
+        System.out.println("LMSGI\t\tDASPGS\t\tSASP\t\tPROGR\t\tLMSGI");
+    }
+    
+}
