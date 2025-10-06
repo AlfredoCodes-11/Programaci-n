4a885c339283e34a3d191ec56e2938ea18c5e4f1
AlfredoCodes-11 <acodsan462@g.educaand.es>
2025-10-06 11:05:51 +0200

    gdf

--- Tema1/ingles.java
+++ Tema1/ingles.java
@@ -2,6 +2,15 @@ package Tema1;
 
 public class ingles {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Computer\tOrdenador");
+        System.out.println("Tree\t\tÁrbol");
+        System.out.println("Car\t\tCoche");
+        System.out.println("Apple\t\tManzana");
+        System.out.println("Dog\t\tPerro");
+        System.out.println("Light\t\tLuz");
+        System.out.println("Book\t\tLibro");
+        System.out.println("Cat\t\tGato");
+        System.out.println("Machine\t\tMáquina");
+        System.out.println("Pyramid\t\tPirámide");
     }
 }
