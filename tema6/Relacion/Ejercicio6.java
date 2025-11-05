bf05307cbe55955a5b9d7d3b96439830b0ed2fa7
AlfredoCodes-11 <acodsan462@g.educaand.es>
2025-11-05 11:53:53 +0100

    hghf

--- tema6/Relacion/Ejercicio6.java
+++ tema6/Relacion/Ejercicio6.java
@@ -9,17 +9,27 @@ public class Ejercicio6 {
 
         numSecre=(int) (Math.random()*101);
 
-
         System.out.println("Estoy pensando un número del 0 al 100, intenta adivinarlo. Tienes 5 oportunidades.");
         System.out.print("Introduce un número: ");
         num=Integer.parseInt(System.console().readLine());
 
-        for (int i =0; i<5; i++){
-            if (num < numSecre){
+        while (num != numSecre && oportu !=0) {
+            oportu--;
+            if (num < numSecre) {
                 System.out.println("El número que estoy pensando es mayor que "+num);
-                oportu1;
-                System.out.println("Te quedan  opor");
+                System.out.println("Te quedan "+ oportu+ " oportunidades.");
+                System.out.print("Introduce un número: ");
+                num=Integer.parseInt(System.console().readLine());
+            } else if (num > numSecre){
+                System.out.println("El número que estoy pensando es menor que "+num);
+                System.out.println("Te quedan "+ oportu+ " oportunidades.");
+                System.out.print("Introduce un número: ");
+                num=Integer.parseInt(System.console().readLine());
+                } else {   
+                System.out.println("Lo siento, no has acertado, el número que estaba pensando era el "+ numSecre);
+                }
             }
-        }
+        System.out.println("¡Enhorabuena! ¡has acertado!");
+        
     }
 }
