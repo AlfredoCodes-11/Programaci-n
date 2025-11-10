b67e44f91b308709b0dcce33496054a9f0f9c1e7
AlfredoCodes-11 <acodsan462@g.educaand.es>
2025-11-10 11:51:21 +0100

    fgfg

--- tema5/Ejercicio7.java
+++ tema5/Ejercicio7.java
@@ -2,6 +2,18 @@ package tema5;
 
 public class Ejercicio7 {
     public static void main(String[] args) {
-        
+        double numero=0;
+        double suma=0;
+        double repeticion=0;
+
+        System.out.println("Este programa calcula la media de los números positivos introducidos.");
+        System.out.println("Para parar, introduzca un número negativo.");
+        System.out.println("Vaya introduciendo números:");
+        while (numero >= 0){
+            numero=Double.parseDouble(System.console().readLine());
+            repeticion++;
+            suma+=numero;
+        } 
+            System.out.printf("La media de los números introducidos es %f",(suma-numero)/(repeticion -1));
     }
 }
